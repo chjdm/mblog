202403abfbb4fd556697a89d55f524451d9f6a25
liuwei <liuwee@sina.com>
2020-10-03 21:16:27 +0800

    1

--- mblog/settings.py
+++ mblog/settings.py
@@ -106,9 +106,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'zh-Hans'
+# LANGUAGE_CODE = 'zh-Hans'
+#
+# TIME_ZONE = 'Asia/shanghai'
 
-TIME_ZONE = 'Asia/shanghai'
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
